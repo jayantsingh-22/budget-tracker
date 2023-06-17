de95f9fb591817cd4cd4fead955988d853e88505
jayantsingh-22 <jayantsingh640@gmail.com>
2023-06-16 23:58:21 -0500

    added:pages dir

---
+++ pages/index.js
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useEffect, useState } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import { EmailAuthProvider, GoogleAuthProvider } from "firebase/auth";
+import {
+  Button,
+  CircularProgress,
+  Container,
+  Dialog,
+  Typography,
+} from "@mui/material";
+import { auth } from "../firebase/firebase";
+import styles from "../styles/landing.module.scss";
+
+const REDIRECT_PAGE = "/dashboard";
+
+// Configure FirebaseUI.
+const uiConfig = {
+  signInFlow: "popup",
+  signInSuccessUrl: REDIRECT_PAGE,
+  signInOptions: [
+    GoogleAuthProvider.PROVIDER_ID,
+    EmailAuthProvider.PROVIDER_ID,
+  ],
+};
+
+export default function Home() {
+  const [login, setLogin] = useState(false);
+
+  const router = useRouter();
+
+  return (
+    <div>
+      <Head>
+        <title>Expense Tracker</title>
+      </Head>
+
+      <main>
+        <Container className={styles.container}>
+          <Typography variant="h1">Welcome to Expense Tracker!</Typography>
+          <Typography variant="h2">
+            Add, view, edit, and delete expenses
+          </Typography>
+          <div className={styles.buttons}>
+            <Button
+              variant="contained"
+              color="secondary"
+              onClick={() => setLogin(true)}
+            >
+              Login / Register
+            </Button>
+          </div>
+          <Dialog open={login} onClose={() => setLogin(false)}>
+            <StyledFirebaseAuth>
+              uiConfig={uiConfig} firebaseAuth={auth}
+            </StyledFirebaseAuth>
+          </Dialog>
+        </Container>
+      </main>
+    </div>
+  );
+}
